f86445efec5b6177f629c1d233b6ebb773b96451
RunarVestmann <runarvestmann@gmail.com>
2020-05-28 13:48:41 +0000

    Reworked the entire project

--- main.js
+++ main.js
@@ -1,58 +1,47 @@
 const mainSection = document.getElementById("main-section");
 
+const menuButton = document.getElementById("menu-button");
+const menu = document.getElementById("menu");
+
 const home = document.getElementById("home");
 const projects = document.getElementById("projects");
 const about = document.getElementById("about");
 const contact = document.getElementById("contact");
 
-let currentDisplay = home;
-
-const description = document.getElementById("logo-description");
-const descriptionOptions = [
-    "Aspiring Programmer, CS Student & Creator",
-    "Just some guy", 
-    "Fellow dreamer",
-    "Icelander",
-    "Soon to be Computer Scientist",
-    "Overthinker",
-    "Genuine dude"
-];
-
-const menuButton = document.getElementById("menu-button");
-const menu = document.getElementById("menu-container");
-
 let isMenuShowing = false;
 
-function generateDescription(){  
-    const previousDescription = description.textContent;
-    description.textContent = descriptionOptions[Math.floor(Math.random() * descriptionOptions.length)];
+let currentDisplay = home;
 
-    //Generate the description again if the previous one gets generated
-    if(description.textContent === previousDescription){
-        generateDescription();
+handleRefresh();
+
+function handleRefresh(){
+    const previousDisplayID = sessionStorage.getItem("displayID");
+    if (previousDisplayID){
+        [home, projects, about, contact].forEach(section =>{
+        if (section.id === previousDisplayID)
+            changeDisplay(section);
+        });
     }
 }
 
-function toggleMenu(){
-    if(!isMenuShowing){
+function toggleMenu(){   
+    if (isMenuShowing)
+        closeMenu(); 
+    else
         openMenu();
-    }
-    else{
-        closeMenu();
-    }
 }
 
 function openMenu(){
-    menuButton.classList = "fa fa-times fa-4x"; //Change the hamburger to be an x
-    menu.style.display = "flex"; //Show menu
-    mainSection.style.opacity = "0.3"; //Lower the opacity of what's behind the menu
+    menuButton.classList = "fa fa-times fa-4x";
+    menu.style.display = "flex"; 
+    mainSection.style.opacity = "0.3";
     isMenuShowing = true;
 }
 
 function closeMenu(){
-    menuButton.classList = "fa fa-bars fa-4x"; //Change the x to be a hamburger icon
-    menu.style.display = "none"; //Hide menu
-    mainSection.style.opacity = "1"; //Set the opacity back to normal
+    menuButton.classList = "fa fa-bars fa-4x";
+    menu.style.display = "none";
+    mainSection.style.opacity = "1";
     isMenuShowing = false;
 }
 
@@ -73,12 +62,11 @@ function showContactPage(){
 }
 
 function changeDisplay(newDisplay){
-    if(currentDisplay === newDisplay){
-        closeMenu();
-        return;
+    if (currentDisplay !== newDisplay){
+        currentDisplay.style.display = "none";
+        newDisplay.style.display = "block";
+        currentDisplay = newDisplay;
     }
-    currentDisplay.style.display = "none";
-    newDisplay.style.display = "block";
-    currentDisplay = newDisplay;
     closeMenu();
+    sessionStorage.setItem("displayID", currentDisplay.id);
 }
